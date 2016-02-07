65e82a1e2a4ab607070174d174769c85fbcf6e1f
Scott Shelton <sshelton@hga-llc.com>
2016-02-06 22:51:28 -0600

    alter PROCEDURE [dbo].[spProjectSummaryInfo_Update_PMReport]

---
+++ Stored Proc Update/alter_PROCEDURE_spProjectSummaryInfo_Update_PMReport_2-6-2016.sql
@@ -0,0 +1,42 @@
+USE [RSManpowerSchDb2014]
+GO
+
+/****** Object:  StoredProcedure [dbo].[spProjectSummaryInfo_Update_PMReport]    Script Date: 2/6/2016 7:58:02 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+alter PROCEDURE [dbo].[spProjectSummaryInfo_Update_PMReport]
+@sumID		int,
+@projID		int
+AS
+declare @no int
+
+select @no = (select count(ProjectID) from DT_ProjectSummaryInfos where ProjectID = @projID and ProjSumID = @sumID)
+if @no > 0
+begin
+
+UPDATE	DT_ProjectSummaryInfos
+SET	[Deleted] = 0 WHERE
+	[ProjSumID] = @sumID and [ProjectID] = @projID
+end
+
+else
+begin
+insert into DT_ProjectSummaryInfos (ProjSumID,ProjectID)	values (@SumID,@ProjID)
+insert into DT_ProjectSummarySch   (ProjSumID,ProjectID)	values (@SumID,@ProjID)
+end
+
+
+
+
+
+
+
+
+
+
