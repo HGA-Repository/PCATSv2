c721bb5d15c074b33792209f21d6dde6826186d9
Scott Shelton <sshelton@hga-llc.com>
2016-02-16 09:36:31 -0600

    FW Fix First Commit

--- MPSBudget/FWS.Designer.cs
+++ MPSBudget/FWS.Designer.cs
@@ -70,7 +70,7 @@ namespace RSMPS
             this.tdbgWS.PreviewInfo.Size = new System.Drawing.Size(0, 0);
             this.tdbgWS.PreviewInfo.ZoomFactor = 75D;
             this.tdbgWS.PrintInfo.PageSettings = ((System.Drawing.Printing.PageSettings)(resources.GetObject("tdbgWS.PrintInfo.PageSettings")));
-            this.tdbgWS.Size = new System.Drawing.Size(1519, 291);
+            this.tdbgWS.Size = new System.Drawing.Size(2049, 291);
             this.tdbgWS.TabAcrossSplits = true;
             this.tdbgWS.TabIndex = 0;
             this.tdbgWS.Text = "c1TrueDBGrid1";
@@ -113,7 +113,7 @@ namespace RSMPS
             this.groupBox1.Controls.Add(this.tdbgWS);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1167, 140);
+            this.groupBox1.Size = new System.Drawing.Size(1697, 140);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Specifications and Procurement";
@@ -121,7 +121,7 @@ namespace RSMPS
             // bttOK
             // 
             this.bttOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bttOK.Location = new System.Drawing.Point(1013, 442);
+            this.bttOK.Location = new System.Drawing.Point(1543, 442);
             this.bttOK.Name = "bttOK";
             this.bttOK.Size = new System.Drawing.Size(80, 30);
             this.bttOK.TabIndex = 2;
@@ -132,7 +132,7 @@ namespace RSMPS
             // bttCancel
             // 
             this.bttCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bttCancel.Location = new System.Drawing.Point(1099, 442);
+            this.bttCancel.Location = new System.Drawing.Point(1629, 442);
             this.bttCancel.Name = "bttCancel";
             this.bttCancel.Size = new System.Drawing.Size(80, 30);
             this.bttCancel.TabIndex = 3;
@@ -147,7 +147,7 @@ namespace RSMPS
             this.groupBox2.Controls.Add(this.tdbgExpense);
             this.groupBox2.Location = new System.Drawing.Point(12, 158);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1167, 278);
+            this.groupBox2.Size = new System.Drawing.Size(1697, 278);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Travel Expenses";
@@ -166,13 +166,13 @@ namespace RSMPS
             this.tdbgExpense.ContextMenuStrip = this.contextMenuStrip2;
             this.tdbgExpense.GroupByCaption = "Drag a column header here to group by that column";
             this.tdbgExpense.Images.Add(((System.Drawing.Image)(resources.GetObject("tdbgExpense.Images"))));
-            this.tdbgExpense.Location = new System.Drawing.Point(6, 19);
+            this.tdbgExpense.Location = new System.Drawing.Point(0, 19);
             this.tdbgExpense.Name = "tdbgExpense";
             this.tdbgExpense.PreviewInfo.Location = new System.Drawing.Point(0, 0);
             this.tdbgExpense.PreviewInfo.Size = new System.Drawing.Size(0, 0);
             this.tdbgExpense.PreviewInfo.ZoomFactor = 75D;
             this.tdbgExpense.PrintInfo.PageSettings = ((System.Drawing.Printing.PageSettings)(resources.GetObject("tdbgExpense.PrintInfo.PageSettings")));
-            this.tdbgExpense.Size = new System.Drawing.Size(2587, 253);
+            this.tdbgExpense.Size = new System.Drawing.Size(2030, 253);
             this.tdbgExpense.TabIndex = 0;
             this.tdbgExpense.Text = "c1TrueDBGrid1";
             this.tdbgExpense.AfterColEdit += new C1.Win.C1TrueDBGrid.ColEventHandler(this.tdbgExpense_AfterColEdit);
@@ -210,7 +210,7 @@ namespace RSMPS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1191, 483);
+            this.ClientSize = new System.Drawing.Size(1721, 483);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.bttCancel);
             this.Controls.Add(this.bttOK);
